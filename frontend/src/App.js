437414c21ba53b511e1c8f5b2e254b4aeda9e3eb
sakshipatel29 <sakshirpatel29062002@gmail.com>
2024-10-15 14:16:58 -0500

    Revert "checkout page is created and data is being stored in order collection."
    This reverts commit 0d5b1f7c0f97e525367b7e09c634a6b31481c0c6.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 import MedicineDetail from './components/MedicineDetail';
-import Checkout from './components/Checkout';
 
 const App = () => {
   return (
@@ -15,7 +14,6 @@ const App = () => {
         <Route path="/register" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/medicine/:id" element={<MedicineDetail />} />
-        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Router>
   );
