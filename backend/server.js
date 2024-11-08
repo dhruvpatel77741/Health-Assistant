7716b7816ebee81446b83ef05460144705030577
dirghpatel3110 <dirghpatel3110@gmail.com>
2024-11-08 14:03:58 -0600

    Code cleanUp

--- backend/server.js
+++ backend/server.js
@@ -1,109 +1,116 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const connectDB = require('./config/db');
-const Ticket = require('./models/Ticket');  // Import the Ticket model
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const connectDB = require("./config/db");
+const Ticket = require("./models/Ticket");
+require("dotenv").config();
 
 const app = express();
 
-connectDB();  // Connect to MongoDB
+connectDB();
 
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'DELETE'],
-    credentials: true 
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/medicines', require('./routes/medicines'));
-app.use('/api/conversations', require('./routes/conversations'));
-app.use('/api/symptoms', require('./routes/symptoms'));
-app.use('/api/cart', require('./routes/cartItem'));
-app.use('/api/orders', require('./routes/order'));
-app.use('/api/clear', require('./routes/clearCart'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/medicines", require("./routes/medicines"));
+app.use("/api/conversations", require("./routes/conversations"));
+app.use("/api/symptoms", require("./routes/symptoms"));
+app.use("/api/cart", require("./routes/cartItem"));
+app.use("/api/orders", require("./routes/order"));
+app.use("/api/clear", require("./routes/clearCart"));
 
-app.post('/chat', async (req, res) => {
-    const userMessage = req.body.message;
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
 
-    try {
-        const response = await axios.post('http://localhost:5005/webhooks/rest/webhook', {
-            sender: 'user1',
-            message: userMessage,
-        });
+  try {
+    const response = await axios.post(
+      "http://localhost:5005/webhooks/rest/webhook",
+      {
+        sender: "user1",
+        message: userMessage,
+      }
+    );
 
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error communicating with chatbot');
-    }
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send("Error communicating with chatbot");
+  }
 });
 
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
-// Set up multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');  // Specify the upload folder
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));  // Append timestamp to the original file name
-  }
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
 const upload = multer({ storage: storage });
 
-// Route to save ticket data
-app.post('/api/tickets', upload.single('ticketImage'), async (req, res) => {
-    const ticketNumber = `Ticket${Date.now()}`; // Unique ticket number
-    const description = req.body.description;  // Description from the request body
-    const imageUrl = req.file ? req.file.path : null; // Path to the uploaded image file
-  
-    // Validate ticket data
-    if (!description) {
-      return res.status(400).json({ message: 'Description is required' });
-    }
-  
-    try {
-      const newTicket = new Ticket({
-        ticket_number: ticketNumber,
-        description: description,
-        image_url: imageUrl
+app.post("/api/tickets", upload.single("ticketImage"), async (req, res) => {
+  const ticketNumber = `Ticket${Date.now()}`;
+  const description = req.body.description;
+  const imageUrl = req.file ? req.file.path : null;
+
+  if (!description) {
+    return res.status(400).json({ message: "Description is required" });
+  }
+
+  try {
+    const newTicket = new Ticket({
+      ticket_number: ticketNumber,
+      description: description,
+      image_url: imageUrl,
+    });
+
+    await newTicket.save();
+
+    res
+      .status(201)
+      .json({
+        message: "Ticket saved successfully",
+        ticketNumber: ticketNumber,
       });
-
-      // Save the ticket to the MongoDB database
-      await newTicket.save();
-
-      res.status(201).json({ message: 'Ticket saved successfully', ticketNumber: ticketNumber });
-    } catch (error) {
-      console.error('Error saving ticket data:', error);
-      res.status(500).json({ message: 'Error saving ticket data', error: error });
-    }
+  } catch (error) {
+    console.error("Error saving ticket data:", error);
+    res.status(500).json({ message: "Error saving ticket data", error: error });
+  }
 });
 
+app.get("/api/tickets/analyze/:ticketNumber", async (req, res) => {
+  try {
+    const ticketNumber = req.params.ticketNumber;
+    const ticket = await Ticket.findOne({ ticket_number: ticketNumber });
 
-// Route to analyze a ticket
-app.get('/api/tickets/analyze/:ticketNumber', async (req, res) => {
-    try {
-        const ticketNumber = req.params.ticketNumber;
-        const ticket = await Ticket.findOne({ ticket_number: ticketNumber });  // Find ticket by ticket number
+    if (!ticket) {
+      return res
+        .status(404)
+        .json({
+          message: "Ticket not found. Please check your ticket number.",
+        });
+    }
 
-        if (!ticket) {
-            return res.status(404).json({ message: 'Ticket not found. Please check your ticket number.' });
-        }
+    const { description, image_url } = ticket;
 
-        const { description, image_url } = ticket;
-
-        // Call OpenAI API to analyze the ticket
-        const gptResponse = await axios.post(
-            'https://api.openai.com/v1/chat/completions',
-            {
-                model: "gpt-4o-mini",
-                messages: [
-                    {
-                        role: "system",
-                        content: `
+    const gptResponse = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "system",
+            content: `
                             You are a customer service assistant for a pharmacy's online web app. Based on the ticket description and image, decide if the customer is eligible for one of the following outcomes:
                             - **Refund Medication**: If the customer received incorrect medication or has a valid reason for returning the medication.
                             - **Replace Medication**: If the customer received the wrong dosage, packaging, or expired medication.
@@ -111,43 +118,42 @@ app.get('/api/tickets/analyze/:ticketNumber', async (req, res) => {
                             - **Escalate to Human Agent**: If the issue cannot be resolved automatically, such as complex prescription errors or disputes.
 
                             **Respond with the decision and a brief reason in one sentence**.
-                        `
-                    },
-                    {
-                        role: "user",
-                        content: `Ticket description: ${description}\nTicket image URL: ${image_url}`
-                    }
-                ]
+                        `,
+          },
+          {
+            role: "user",
+            content: `Ticket description: ${description}\nTicket image URL: ${image_url}`,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
+    const decision = gptResponse.data.choices[0].message.content.trim();
 
-        const decision = gptResponse.data.choices[0].message.content.trim();
-        
-        ticket.decision = decision;  // Update the decision field
-        await ticket.save();  // Save the updated ticket
+    ticket.decision = decision;
+    await ticket.save();
 
-        res.status(200).json({
-            message: 'Ticket analyzed successfully',
-            ticketNumber: ticketNumber,
-            decision: decision
-        });
-    } catch (error) {
-        console.error('Error with OpenAI API call:', error);
-        res.status(500).json({
-            message: 'Error analyzing the ticket',
-            error: error.response ? error.response.data : error.message
-        });
-    }
+    res.status(200).json({
+      message: "Ticket analyzed successfully",
+      ticketNumber: ticketNumber,
+      decision: decision,
+    });
+  } catch (error) {
+    console.error("Error with OpenAI API call:", error);
+    res.status(500).json({
+      message: "Error analyzing the ticket",
+      error: error.response ? error.response.data : error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- frontend/src/components/CustomerService.jsx
+++ frontend/src/components/CustomerService.jsx
@@ -1,131 +1,147 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './CSS/CustomerService.css';
-import Navbar from './Navbar';
+import React, { useState } from "react";
+import axios from "axios";
+import "./CSS/CustomerService.css";
+import Navbar from "./Navbar";
 
 function CustomerService() {
-  const [selectedOption, setSelectedOption] = useState('submit'); // 'submit' or 'analyze'
-  const [ticketNumber, setTicketNumber] = useState('');
-  const [description, setDescription] = useState('');
+  const [selectedOption, setSelectedOption] = useState("submit");
+  const [ticketNumber, setTicketNumber] = useState("");
+  const [description, setDescription] = useState("");
   const [ticketImage, setTicketImage] = useState(null);
-  const [responseMessage, setResponseMessage] = useState('');
+  const [responseMessage, setResponseMessage] = useState("");
   const [analysisResult, setAnalysisResult] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const baseURL = process.env.REACT_APP_API_BASE_URL;
 
-  // Handle ticket number change for both forms
   const handleTicketNumberChange = (e) => {
     setTicketNumber(e.target.value);
   };
 
-  // Handle description change
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value);
   };
 
-  // Handle image file change
   const handleImageChange = (e) => {
     setTicketImage(e.target.files[0]);
   };
-    
-  // Handle submit ticket form
+
   const handleSubmitTicket = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('description', description);
-    formData.append('ticketImage', ticketImage);
+    formData.append("description", description);
+    formData.append("ticketImage", ticketImage);
 
     try {
       const response = await axios.post(`${baseURL}/tickets`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
       });
-      setTicketNumber(response.data.ticketNumber); // Display generated ticket number
+      setTicketNumber(response.data.ticketNumber);
       setResponseMessage(response.data.message);
     } catch (error) {
-      setResponseMessage('Error submitting ticket');
+      setResponseMessage("Error submitting ticket");
       console.error(error);
     }
   };
 
-  // Handle analyze ticket
   const handleAnalyzeTicket = async () => {
     try {
-      const response = await axios.get(`http://localhost:3006/api/tickets/analyze/${ticketNumber}`);
+      const response = await axios.get(
+        `${baseURL}/tickets/analyze/${ticketNumber}`
+      );
       setAnalysisResult(response.data.decision);
-      setErrorMessage('');
+      setErrorMessage("");
     } catch (error) {
       setAnalysisResult(null);
-      setErrorMessage('Error analyzing ticket');
+      setErrorMessage("Error analyzing ticket");
       console.error(error);
     }
   };
 
   return (
     <>
-    <Navbar/>
-    <div className="customer-service">
-      <h2 className="customer-service__title">Customer Service Dashboard</h2>
+      <Navbar />
+      <div className="customer-service">
+        <h2 className="customer-service__title">Customer Service Dashboard</h2>
 
-      <select
-        className="customer-service__dropdown"
-        value={selectedOption}
-        onChange={(e) => setSelectedOption(e.target.value)}
-      >
-        <option value="submit">Submit a Ticket</option>
-        <option value="analyze">Analyze a Ticket</option>
-      </select>
+        <select
+          className="customer-service__dropdown"
+          value={selectedOption}
+          onChange={(e) => setSelectedOption(e.target.value)}
+        >
+          <option value="submit">Submit a Ticket</option>
+          <option value="analyze">Analyze a Ticket</option>
+        </select>
 
-      {selectedOption === 'submit' && (
-        <div className="customer-service__submit-section">
-          <h3 className="customer-service__subtitle">Submit a Ticket</h3>
-          <form className="customer-service__form" onSubmit={handleSubmitTicket}>
-            <label className="customer-service__label">
-              Ticket Number: {ticketNumber || 'Generating automatically after submission'}
-            </label>
-            <textarea
-              className="customer-service__textarea"
-              value={description}
-              onChange={handleDescriptionChange}
-              placeholder="Describe your issue"
+        {selectedOption === "submit" && (
+          <div className="customer-service__submit-section">
+            <h3 className="customer-service__subtitle">Submit a Ticket</h3>
+            <form
+              className="customer-service__form"
+              onSubmit={handleSubmitTicket}
+            >
+              <label className="customer-service__label">
+                Ticket Number:{" "}
+                {ticketNumber || "Generating automatically after submission"}
+              </label>
+              <textarea
+                className="customer-service__textarea"
+                value={description}
+                onChange={handleDescriptionChange}
+                placeholder="Describe your issue"
+                required
+              />
+              <input
+                type="file"
+                className="customer-service__file-input"
+                onChange={handleImageChange}
+              />
+              <button type="submit" className="customer-service__submit-button">
+                Submit Ticket
+              </button>
+            </form>
+            {responseMessage && (
+              <p className="customer-service__response">{responseMessage}</p>
+            )}
+            {ticketNumber && (
+              <p className="customer-service__ticket-number">
+                Your Ticket Number: {ticketNumber}
+              </p>
+            )}
+          </div>
+        )}
+
+        {selectedOption === "analyze" && (
+          <div className="customer-service__analyze-section">
+            <h3 className="customer-service__subtitle">Analyze a Ticket</h3>
+            <input
+              type="text"
+              className="customer-service__input"
+              value={ticketNumber}
+              onChange={handleTicketNumberChange}
+              placeholder="Enter ticket number"
               required
             />
-            <input
-              type="file"
-              className="customer-service__file-input"
-              onChange={handleImageChange}
-            />
-            <button type="submit" className="customer-service__submit-button">Submit Ticket</button>
-          </form>
-          {responseMessage && <p className="customer-service__response">{responseMessage}</p>}
-          {ticketNumber && <p className="customer-service__ticket-number">Your Ticket Number: {ticketNumber}</p>}
-        </div>
-      )}
-
-      {selectedOption === 'analyze' && (
-        <div className="customer-service__analyze-section">
-          <h3 className="customer-service__subtitle">Analyze a Ticket</h3>
-          <input
-            type="text"
-            className="customer-service__input"
-            value={ticketNumber}
-            onChange={handleTicketNumberChange}
-            placeholder="Enter ticket number"
-            required
-          />
-          <button className="customer-service__analyze-button" onClick={handleAnalyzeTicket}>Analyze Ticket</button>
-          {analysisResult && (
-            <div className="customer-service__analysis-result">
-              <h4>Analysis Result:</h4>
-              <p>{analysisResult}</p>
-            </div>
-          )}
-          {errorMessage && <p className="customer-service__error-message">{errorMessage}</p>}
-        </div>
-      )}
-    </div>
+            <button
+              className="customer-service__analyze-button"
+              onClick={handleAnalyzeTicket}
+            >
+              Analyze Ticket
+            </button>
+            {analysisResult && (
+              <div className="customer-service__analysis-result">
+                <h4>Analysis Result:</h4>
+                <p>{analysisResult}</p>
+              </div>
+            )}
+            {errorMessage && (
+              <p className="customer-service__error-message">{errorMessage}</p>
+            )}
+          </div>
+        )}
+      </div>
     </>
   );
 }
